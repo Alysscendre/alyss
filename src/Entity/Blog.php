dbc2252a22a0be3df836c1f12c32f5741b167a8e
AlyssCendre <77501571+Alysscendre@users.noreply.github.com>
2022-05-27 10:03:51 +0200

    les débuts

---
+++ src/Entity/Blog.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BlogRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation\Slug;
+use Gedmo\Mapping\Annotation\Timestampable;
+
+#[ORM\Entity(repositoryClass: BlogRepository::class)]
+class Blog
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $title;
+
+    #[ORM\Column(type: 'text')]
+    private $content;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    #[Timestampable(on: 'create')]
+    private $created_at;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Slug(fields: ['title'])]
+    private $slug;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+}
